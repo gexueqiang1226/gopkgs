a26ea0199a44a69fff590a82ef9e4ddd764ebea4
Administrator <gexueqiang1226@gmail.com>
2022-08-15 23:30:00 +0800

    Change Hi to allown lang

--- say.go
+++ say.go
@@ -1,7 +1,17 @@
 package gopkgs
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Say(name string) string {
-	return fmt.Sprintf("Hi, %s!!!", name)
+func Say(name string, lang string) (string, error) {
+	switch lang {
+	case "zh":
+		return fmt.Sprintf("你好, %s!!!", name), nil
+		break
+	case "en":
+		return fmt.Sprintf("Hi, %s!!!", name), nil
+	}
+	return "", errors.New("unknown language")
 }
